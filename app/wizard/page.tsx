ff794f3c6b41094518871d96149eabedac06bd9b
Shreyas Gadhe <gadheshreyas@gmail.com>
2025-07-29 00:14:18 +0530

    working on currency select option

--- app/wizard/page.tsx
+++ app/wizard/page.tsx
@@ -1,5 +1,16 @@
+import { CurrencyComboBox } from "@/components/CurrencyComboBox";
+import Logo from "@/components/logo";
+import { Button } from "@/components/ui/button";
+import {
+  Card,
+  CardContent,
+  CardDescription,
+  CardHeader,
+  CardTitle,
+} from "@/components/ui/card";
 import { Separator } from "@/components/ui/separator";
 import { currentUser } from "@clerk/nextjs/server";
+import Link from "next/link";
 import { redirect } from "next/navigation";
 import React from "react";
 
@@ -9,17 +20,37 @@ async function page() {
     redirect("/sign-in");
   }
   return (
-    <div className="container flex flex-col max-w-2xl items-center justify-between gap-4">
-      <h1 className="text-center text-3xl">
-        Welcome, <span className="ml-2 font-bold">{user.firstName}!</span>
-      </h1>
-      <h2 className="mt-4 text-center text-base tex-muted-foreground">
-        Let &apos;s get started by setting up your currency
-      </h2>
-      <h3 className="mt-2 text-center text-sm text-muted-foreground">
-        You can change these settings at any time
-      </h3>
+    <div className=" px-4 container flex flex-col max-w-2xl items-center justify-between gap-4">
+      <div>
+        <h1 className="text-center text-3xl">
+          Welcome, <span className="ml-2 font-bold">{user.firstName}!</span>
+        </h1>
+        <h2 className="mt-4 text-center text-base tex-muted-foreground">
+          Let&apos;s get started by setting up your currency
+        </h2>
+        <h3 className="mt-2 text-center text-sm text-muted-foreground">
+          You can change these settings at any time
+        </h3>
+      </div>
       <Separator />
+      <Card className="w-full">
+        <CardHeader>
+          <CardTitle>Currency</CardTitle>
+          <CardDescription>
+            Set your default currency for transcations
+          </CardDescription>
+        </CardHeader>
+        <CardContent>
+          <CurrencyComboBox />
+        </CardContent>
+      </Card>
+      <Separator />
+      <Button asChild className="w-full">
+        <Link href={"/"}>I&apos;m done. Take me to the Dashboard!</Link>
+      </Button>
+      <div className="mt-8">
+        <Logo />
+      </div>
     </div>
   );
 }
