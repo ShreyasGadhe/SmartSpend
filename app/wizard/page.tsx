9946edde3d422c9307dc513440c1c9b38041d0b9
Shreyas Gadhe <gadheshreyas@gmail.com>
2025-07-28 22:37:20 +0530

    working on wizard page and fixing shadcn issues

---
+++ app/wizard/page.tsx
@@ -0,0 +1,27 @@
+import { Separator } from "@/components/ui/separator";
+import { currentUser } from "@clerk/nextjs/server";
+import { redirect } from "next/navigation";
+import React from "react";
+
+async function page() {
+  const user = await currentUser();
+  if (!user) {
+    redirect("/sign-in");
+  }
+  return (
+    <div className="container flex flex-col max-w-2xl items-center justify-between gap-4">
+      <h1 className="text-center text-3xl">
+        Welcome, <span className="ml-2 font-bold">{user.firstName}!</span>
+      </h1>
+      <h2 className="mt-4 text-center text-base tex-muted-foreground">
+        Let &apos;s get started by setting up your currency
+      </h2>
+      <h3 className="mt-2 text-center text-sm text-muted-foreground">
+        You can change these settings at any time
+      </h3>
+      <Separator />
+    </div>
+  );
+}
+
+export default page;
